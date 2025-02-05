9225fe4bbc37b3916dadacf6b274c40cd69699b2
smith-bimal <golokbihareebeemal@gmail.com>
2025-02-06 01:00:37 +0530

    fixed auth issue

--- admin/auth/js/Adminlogin.js
+++ admin/auth/js/Adminlogin.js
@@ -5,16 +5,19 @@ let users = [];
 async function fetchUserDetails() {
     let res = await fetch("https://b42web03webwizards-default-rtdb.asia-southeast1.firebasedatabase.app/users.json");
     let data = await res.json();
-    users = data;
+    users = Object.entries(data);
 }
 
 fetchUserDetails();
 
 document.getElementById("login-form").addEventListener("submit", function (event) {
     event.preventDefault();
-    let currentUser = users.filter(user => user.email === email.value);
+    console.log(users)
+    let currentUser = users.filter(user => user[1].email === email.value);
 
-    if (currentUser.length === 0 || currentUser[0].password !== password.value) {
+    console.log(email.value, password.value)
+
+    if (currentUser.length === 0 || currentUser[0][1].password !== password.value) {
         alert("Invalid Credentials");
         return;
     }
