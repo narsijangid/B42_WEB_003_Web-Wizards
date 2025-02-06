c0acbcf90265470346611f3f47dfd137deeabd41
narsi-jangid <narsijangid01@gmail.com>
2025-02-06 10:40:20 +0530

    Completed search functionality in landing page and orderlist in admin panel

---
+++ js/index.js
@@ -0,0 +1,38 @@
+let productDiv = document.getElementById("product-section");
+
+      
+(async function() {
+    let API = await fetch("https://b42web03webwizards-default-rtdb.asia-southeast1.firebasedatabase.app/products.json");
+    let response = await API.json();
+    displayData(response);
+})();
+
+function displayData(response) {
+    response.forEach((item) => {
+        let singleProductDiv = document.createElement("div");
+        singleProductDiv.setAttribute("id", "single-product-div");
+        singleProductDiv.innerHTML = `
+            <div id="product-image-div">
+                <img src="${item.thumbnail}" />
+            </div>
+            <p id="product-brand">${item.brand}</p>
+            <h3>${item.title}</h3>
+            <div id="item-price-rate">
+                <p>${item.price}</p>
+                <p><img src=""/>${item.rating}</p>
+            </div>`;
+        productDiv.append(singleProductDiv);
+    });
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('search-button').addEventListener('click', function() {
+        let searchValue = document.querySelector('input').value;
+        if (searchValue) {
+            window.location.href = `./users/products/Searchlist.html?search=${encodeURIComponent(searchValue)}`;
+        }
+    });
+});
+
+
---
+++ users/products/Orderlist.js
@@ -0,0 +1,23 @@
+const sidebar = document.getElementById('sidebar');
+const closeBtn = document.getElementById('close-btn');
+const openSidebarBtn = document.getElementById('open-sidebar-btn');
+const reopenSidebarBtn = document.getElementById('reopen-sidebar-btn');
+const mainContent = document.getElementById('main-content');
+
+closeBtn.addEventListener('click', () => {
+    sidebar.classList.add('closed');
+    mainContent.classList.add('sidebar-closed');
+    reopenSidebarBtn.style.display = 'block'; 
+});
+
+openSidebarBtn.addEventListener('click', () => {
+    sidebar.classList.remove('closed');
+    mainContent.classList.remove('sidebar-closed');
+    reopenSidebarBtn.style.display = 'none'; 
+});
+
+reopenSidebarBtn.addEventListener('click', () => {
+    sidebar.classList.remove('closed');
+    mainContent.classList.remove('sidebar-closed');
+    reopenSidebarBtn.style.display = 'none';
+});
