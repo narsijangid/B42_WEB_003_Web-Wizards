02462875355596e0a8ca5a323d1bbdf10a78e77a
Sai Maharana <saimaharana24@gmail.com>
2025-02-08 19:52:13 +0530

    CRUD operation on cart and payment page, fixed the UI of cart and payment

--- js/navbar.js
+++ js/navbar.js
@@ -16,3 +16,17 @@ document.addEventListener("DOMContentLoaded", function () {
         window.location.href = "B42_WEB_003_Web-Wizards/index.html";
     })
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Select the shopping cart icon by its class name
+    const cartIcon = document.querySelector(".ri-shopping-cart-line");
+    
+    // Check if the element exists to avoid errors
+    if (cartIcon) {
+      cartIcon.addEventListener("click", () => {
+        // Redirect to the new page
+        window.location.href = "B42_WEB_003_Web-Wizards/users/cartAndpayment/cartAndPayment.html"; // Change URL as needed
+      });
+    }
+  });
+  
